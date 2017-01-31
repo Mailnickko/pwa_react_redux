f316bc2a60cd586609840c8d01ae6b9e72451631
Nicholas Ko <mailnickko@gmail.com>
2017-01-30 16:04:14 -0800

    (feat) Wire up action creators

--- src/actions/actionTypes.js
+++ src/actions/actionTypes.js
@@ -1,1 +1,4 @@
 export const INIT_APP = 'INIT_APP';
+export const QUERY_UPDATED = 'QUERY_UPDATED';
+export const FETCH_QUERY = 'FETCH_QUERY';
+export const FETCH_ERROR = 'FETCH_ERROR';
--- src/actions/actions.js
+++ src/actions/actions.js
@@ -6,3 +6,33 @@ export function initApp() {
     type: types.INIT_APP
   }
 }
+
+export function updateQuery(query) {
+  return {
+    type: types.QUERY_UPDATED,
+    query
+  }
+}
+
+export function searchQuery(query) {
+  const subreddit = query !== 'new' && query !== 'hot' ? `r/${query}` : query;
+  const api = `https://www.reddit.com/${subreddit}.json?limit=10`;
+  return dispatch => {
+    if ('caches' in window) {
+      caches.match(api)
+        .then(resp => {
+          if (resp) {
+            console.log('response found', resp)
+            // TODO: Send dispatch with jsonified resp
+            // No Reponse given => Response not properly saved in cache
+          } else {
+            console.log('NO RESPONSE', resp);
+          }
+        })
+    }
+    fetch(api)
+      .then(resp => resp.json())
+      .then(({data}) => dispatch({ type: types.FETCH_QUERY, posts: data.children }))
+      .catch(error => dispatch({ type: types.FETCH_ERROR, error }));
+  }
+}
