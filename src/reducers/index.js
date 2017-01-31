b8468dcfd3c21140bf5bd52285b8e57e7f5224fe
Nicholas Ko <mailnickko@gmail.com>
2017-01-30 16:13:07 -0800

    (feat) Wire up reducers to rootReducer and expose store api

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux';
 import initReducer from './initReducer';
+import searchReducer from './searchReducer';
 
 const rootReducer = combineReducers({
   init: initReducer,
+  search: searchReducer,
   routing: routerReducer,
 });
 
