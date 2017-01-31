8a8ac5c803c197420638a02243fa8e890eae077c
Nicholas Ko <mailnickko@gmail.com>
2017-01-30 16:04:52 -0800

    (feat) Lay base for root component

--- src/components/containers/App.js
+++ src/components/containers/App.js
@@ -1,39 +1,91 @@
 import React, { Component } from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import { grey100, grey300, grey500, pink500, blue700, blue800, blue600, white, darkBlack, fullBlack } from 'material-ui/styles/colors';
+import { fade } from 'material-ui/utils/colorManipulator';
+import Paper from 'material-ui/Paper';
+
 
 import * as actions from '../../actions/actions';
+import Header from './Header';
+import PostList from '../presentational/PostList';
+
+const muiTheme = getMuiTheme({
+  palette: {
+    primary1Color: blue700,
+    primary2Color: blue800,
+    primary3Color: blue600,
+    accent1Color: pink500,
+    accent2Color: grey100,
+    accent3Color: grey500,
+    textColor: darkBlack,
+    alternateTextColor: white,
+    canvasColor: white,
+    borderColor: grey300,
+    disabledColor: fade(darkBlack, 0.3),
+    pickerHeaderColor: '#607D8B',
+    clockCircleColor: fade(darkBlack, 0.07),
+    shadowColor: fullBlack,
+  },
+  appBar: {
+    height: 50,
+  },
+});
 
 class App extends Component {
 
   constructor() {
     super();
 
-    this.handleInitClick = this.handleInitClick.bind(this);
+    this.renderPosts = this.renderPosts.bind(this);
   }
 
-  handleInitClick() {
-    this.props.initApp();
+  componentWillMount() {
+    this.props.searchQuery(this.props.search.query);
+  }
+
+  renderPosts() {
+    const subreddit = this.props.search.query;
+    if (this.props.search.posts[subreddit] && this.props.search.posts[subreddit].length) {
+      return (
+        <div>
+          <h4 style={{textAlign: 'center'}}>Results from r/{subreddit}</h4>
+            <Paper style={{width: '90%', margin: '0 auto'}} zDepth={2}>
+              { this.props.search.posts[subreddit].map((post, i) =>
+                  <PostList
+                    key={i}
+                    post={post.data}
+                  />
+              )}
+            </Paper>
+        </div>
+      );
+    } else {
+      return <h3>Loading posts for r/{subreddit}</h3>
+    }
   }
 
   render() {
     return (
-      <div>
-        <h1>Hello World</h1>
+      <MuiThemeProvider muiTheme={ muiTheme }>
+        <div>
+          <Header />
 
-        <button onClick={this.handleInitClick}>Click to check dispatch</button>
+          { this.renderPosts() }
 
-        <h3>{ this.props.init }</h3>
-
-        { this.props.children }
-      </div>
+          { this.props.children }
+        </div>
+      </MuiThemeProvider>
     );
   }
 };
 
 function mapStateToProps(state) {
   return {
-    init: state.init.message
+    search: state.search,
+    error: state.search.error
   };
 }
 
