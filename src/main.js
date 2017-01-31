925c97e9420a0c3910ef65ea76460c199576c4ea
Nicholas Ko <mailnickko@gmail.com>
2017-01-30 16:07:11 -0800

    (feat) Configure base entry point for webpack

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@
 import { render } from 'react-dom';
 import React from 'react';
 import { Router } from 'react-router';
+import injectTapEventPlugin from 'react-tap-event-plugin';
 import { Provider } from 'react-redux';
 
 import { store, history } from './store/configureStore';
@@ -10,6 +11,15 @@ import { store, history } from './store/configureStore';
 import './styles/styles.css';
 import rootReducer from './reducers';
 import routes from './routes';
+
+injectTapEventPlugin();
+
+if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
+  // SW will only get registered to set path, so keep it on root to extend benefits
+  navigator.serviceWorker.register('../service-worker.js')
+    .then(() => console.log('Registered Service Worker'))
+    .catch( err => console.log('Error: ', err));
+}
 
 const router = (
   <Provider store={store}>
